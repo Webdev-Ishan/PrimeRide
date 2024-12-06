90235e67e3a40d469eb4d3cb26807c5e7e2b307b
Ishan Saini <ishansaini0105@gmail.com>
2024-12-06 14:26:39 +0530

    Initial server setup complete

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const dotenv= require('dotenv');
+dotenv.config();
+const cors= require('cors');
+
+const app= express();
+
+app.use(cors)
+
+
+module.exports= app;
